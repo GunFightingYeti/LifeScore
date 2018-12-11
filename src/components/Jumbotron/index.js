3636370e5e9bb30bc506e7196e83fcdabefec840
Tim Lewis <timothydlewis@icloud.com>
2018-12-11 00:30:13 -0700

    Building out file structure and basic site.

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../app.css";
+
+function Jumbotron(props) {
+  return (
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container background-box">
+            <h1>Reactive News</h1>
+            <p>Hi</p>
+        </div>
+    </div>
+    );  
+}
+
+export default Jumbotron;
