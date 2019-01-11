1ddf6f7bf5af10f1511bae2b4a263a959f274032
Tim Lewis <timothydlewis@icloud.com>
2019-01-10 18:23:26 -0700

    More margin changes

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -19,7 +19,7 @@ function Login(props) {
                   <div className="form-group">
                       <label htmlFor="exampleInputEmail1">Email address</label>
                       <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required/>
-                      <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                      <small id="emailHelp" className="form-text text-muted ml-1">We'll never share your email with anyone else.</small>
                       <div className="invalid-feedback">
                           Please provide a valid Email address.
                       </div>
