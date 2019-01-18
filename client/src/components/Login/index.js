f04ba7bc767f75bd40750143f8ee14126a57da13
Tim Lewis <timothydlewis@icloud.com>
2019-01-17 17:34:09 -0700

    Cleaning up code

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -12,10 +12,10 @@ function Login(props) {
 
               <div className="col-12 results p-4">
               
-              <div className="col-12">
-                <h1 className="mb-4">Login</h1>
-                <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="true"></div>
-              </div>
+                <div className="col-12">
+                  <h1 className="mb-4">Login</h1>
+                  <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="true"></div>
+                </div>
               
                 <h1 className="my-4">How do I play this game?</h1>
                 <h4>1. Login</h4>
